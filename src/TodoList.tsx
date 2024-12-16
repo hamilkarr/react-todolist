61a6281d6ece333c63e37c8cb200af3c352d7189
hamilkarr <12uclock@gmail.com>
2024-12-17 06:06:22 +0900

    todoList 인풋 수정 도커 연습 완료

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -4,30 +4,19 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 // Zod 스키마 정의
 const formSchema = z.object({
-  email: z.string()
-    .email("유효한 이메일 주소를 입력해주세요"),
-  firstName: z.string()
-    .min(1, "이름을 입력해주세요"),
-  lastName: z.string()
-    .min(1, "성을 입력해주세요"),
-  password: z.string()
-    .min(8, "비밀번호는 최소 8자 이상이어야 합니다"),
-  confirmPassword: z.string()
-    .min(8, "비밀번호는 최소 8자 이상이어야 합니다")
-}).refine((data) => data.password === data.confirmPassword, {
-  message: "비밀번호가 일치하지 않습니다",
-  path: ["confirmPassword"]
-});
+  todo: z.string().min(1, { message: "할 일을 입력해주세요" })
+})
 
 type FormSchema = z.infer<typeof formSchema>;
 
 const ToDoList = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormSchema>({
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm<FormSchema>({
     resolver: zodResolver(formSchema)
   });
 
   const onSubmit = (data: FormSchema) => {
     console.log(data);
+    setValue('todo', '');
   };
 
   return (
@@ -42,39 +31,10 @@ const ToDoList = () => {
       }}
     >
       <input
-        {...register("email")}
-        placeholder="Email"
+        {...register('todo', { required: true })}
+        placeholder="Write a to do"
       />
-      {errors.email && <span style={{ color: "red" }}>{errors.email.message}</span>}
-      
-      <input
-        {...register("firstName")}
-        placeholder="First Name"
-      />
-      {errors.firstName && <span style={{ color: "red" }}>{errors.firstName.message}</span>}
-      
-      <input
-        {...register("lastName")}
-        placeholder="Last Name"
-      />
-      {errors.lastName && <span style={{ color: "red" }}>{errors.lastName.message}</span>}
-      
-      <input
-        type="password"
-        {...register("password")}
-        placeholder="Password"
-      />
-      {errors.password && <span style={{ color: "red" }}>{errors.password.message}</span>}
-      
-      <input
-        type="password"
-        {...register("confirmPassword")}
-        placeholder="Confirm Password"
-      />
-      {errors.confirmPassword && (
-        <span style={{ color: "red" }}>{errors.confirmPassword.message}</span>
-      )}
-      
+      {errors.todo && <span style={{ color: "red" }}>{errors.todo.message}</span>}
       <button type="submit">Add</button>
     </form>
   );
